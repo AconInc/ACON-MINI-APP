171117846cc92a0d84d59aa6cc5d5fd6ad307ea1
Yurim Kim <yurim830@gmail.com>
2025-10-30 01:05:39 +0900

    [Add,Feat] watch-ad 뷰 구현 (#3)

--- acon-mini-app/src/pages/watch-ad.tsx
+++ acon-mini-app/src/pages/watch-ad.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ScrollView, View } from 'react-native';
 
-import { createRoute, Image } from '@granite-js/react-native';
+import { createRoute, Lottie } from '@granite-js/react-native';
 import { Button, Text } from '@toss/tds-react-native';
 
-import { globalStyles as styles, shakeStyles } from 'styles/styles';
+import { globalStyles } from 'styles/globalStyles';
+import { watchAdStyles as styles } from 'styles/watchAdStyles';
 import { useSafeAreaInsets } from '@granite-js/native/react-native-safe-area-context';
 
 export const Route = createRoute('/watch-ad', {
@@ -23,44 +24,42 @@ function WatchAd() {
   };
 
   return (
-    <View style={[styles.container]}>
+    <View style={[globalStyles.container]}>
       <ScrollView>
-        <View style={styles.titleView}>
+        <View style={[globalStyles.titleView, { marginBottom: 32 }]}>
           <Text typography="st5" fontWeight="semibold" color="#111">
-            폰을 흔들어
+            짧은 광고를 시청하고{'\n'}
             <Text typography="st5" fontWeight="semibold" color="#FF4A02">
-              도토리
+              맛집
             </Text>
-            를{'\n'}떨어뜨려 보세요
+            을 확인해 보세요
           </Text>
         </View>
 
-        <View style={shakeStyles.imageContainer}>
-          <Image
-            source={{
-              uri: 'https://picsum.photos/200',
+        <View style={styles.lottieContainer}>
+          <Lottie
+            width={'100%'}
+            height={270}
+            src="https://raw.githubusercontent.com/AconInc/ACON-MINI-APP/refs/heads/feature/%233/assets/drop5Acorn.json"
+            autoPlay={true}
+            loop={false}
+            onAnimationFailure={() => {
+              console.log('Animation Failed');
             }}
-            style={{
-              width: 200,
-              height: 200,
-              borderWidth: 1,
-            }}
-            onError={() => {
-              console.log('이미지 로드 실패');
+            onAnimationFinish={() => {
+              console.log('Animation Finished');
             }}
           />
-        </View>
-
-        <View style={shakeStyles.bubbleContainer}>
-          <Text typography="t5" fontWeight="regular" color="#111">
-            {`현재 있는 지역을 기준으로\n최고의 맛집을 한 곳 추천해 드려요`}
-          </Text>
         </View>
       </ScrollView>
 
       {/* Next button container */}
-      <Button display={'block'} viewStyle={[styles.buttonBlock, { marginBottom: insets.bottom }]} onPress={handleNext}>
-        다음
+      <Button
+        display={'block'}
+        viewStyle={[globalStyles.buttonBlock, { marginBottom: insets.bottom }]}
+        onPress={handleNext}
+      >
+        맛집 확인하기
       </Button>
     </View>
   );
