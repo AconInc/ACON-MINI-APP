ca48f08203a9e55dab1261cdbb4e8cb490deed34
Yurim Kim <yurim830@gmail.com>
2025-11-09 17:31:52 +0900

    [Chore] 에셋 추가 (#7)

--- acon-mini-app/src/constants/assets.ts
+++ acon-mini-app/src/constants/assets.ts
@@ -4,4 +4,8 @@ export const LOTTIES = {
 
 export const IMAGES = {
   AppIcon: 'https://acon-bucket.s3.ap-northeast-2.amazonaws.com/app-in-toss/app-icon.png',
+
+  HeadAche: 'https://acon-bucket.s3.ap-northeast-2.amazonaws.com/app-in-toss/img-headache.png',
+
+  Loading: 'https://acon-bucket.s3.ap-northeast-2.amazonaws.com/app-in-toss/loading.png',
 } as const;
