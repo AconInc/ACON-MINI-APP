4418bd60eb7c90f559566ff65e7de313239d7e45
Yurim Kim <yurim830@gmail.com>
2025-11-09 17:42:11 +0900

    [Add,Feat] 실패 뷰 구현 (#7)

---
+++ acon-mini-app/src/pages/failed.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+import { createRoute, Image } from '@granite-js/react-native';
+import { Text } from '@toss/tds-react-native';
+import { useSafeAreaInsets } from '@granite-js/native/react-native-safe-area-context';
+
+import { globalStyles } from 'styles/globalStyles';
+import { watchAdStyles as styles } from 'styles/watchAdStyles';
+import { IMAGES } from 'constants/assets';
+
+export const Route = createRoute('/failed', {
+  validateParams: (params) => params,
+  component: Loading,
+});
+
+function Loading() {
+  // 🔹 다음 버튼 UI
+  const insets = useSafeAreaInsets();
+
+  // 🔹 '다시 추천받기' 버튼 action
+  const navigation = Route.useNavigation();
+  const handleNext = async () => {
+    console.log('다시 시도하기 클릭됨');
+    navigation.navigate('/loading');
+    // TODO: 재시도 로직 추가
+  };
+
+  return (
+    <View style={[globalStyles.container]}>
+      {/* Title */}
+      <View style={globalStyles.titleView}>
+        <Text typography="st5" fontWeight="semibold" color="#111">
+          이런..{'\n'}장소를 찾는데 문제가 생겼어요
+        </Text>
+      </View>
+
+      {/* Image */}
+      <View style={styles.lottieContainer}>
+        <Image
+          source={{ uri: IMAGES.HeadAche }}
+          style={{
+            width: 220,
+            height: 220,
+          }}
+          onError={() => {
+            console.log('이미지 로드 실패');
+          }}
+        />
+      </View>
+
+      {/* Next button container */}
+      <TouchableOpacity
+        onPress={handleNext}
+        activeOpacity={0.9}
+        style={[globalStyles.greyButtonContainer, { marginBottom: insets.bottom }]}
+      >
+        <Text typography="t5" fontWeight="semibold" color="#fff" textAlign="center">
+          다시 시도하기
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
