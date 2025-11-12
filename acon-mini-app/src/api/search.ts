d60fa4df8722651a0ae8ea1e912ac1e5f13f481c
Yurim Kim <yurim830@gmail.com>
2025-11-13 04:40:07 +0900

    [Feat] postSpot API 구현 (#9)

--- acon-mini-app/src/api/search.ts
+++ acon-mini-app/src/api/search.ts
@@ -1,6 +1,11 @@
 import { Alert } from 'react-native';
 
+import { SECRET_CONFIG } from 'config/secretConfig';
+import { useSpotStore } from 'store/spotStore';
+
 export const postSearch = () => {
+  const { setSpot } = useSpotStore((state) => state.actions);
+  
   const handleNext = async (value: string) => {
     if (!value.trim()) {
       Alert.alert('알림', '검색어를 입력해주세요.');
@@ -8,20 +13,20 @@ export const postSearch = () => {
     }
 
     try {
-      const response = await fetch('https://api.example.com/search', {
+      const response = await fetch(`${SECRET_CONFIG.BASE_URL}/api/v1/app-in-toss/spots`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query: value }),
+        body: JSON.stringify({ userInput: value }),
       });
 
       if (!response.ok) throw new Error('서버 오류');
       const result = await response.json();
+      setSpot({ id: 1, spotName: result.spotName, category: '카페' });
 
-      // Alert.alert('성공', '검색 요청이 전송되었습니다!');
-      // console.log('검색 결과:', result);
+      console.log('검색 결과:', result);
     } catch (error) {
-      // console.error(error);
-      // Alert.alert('오류', '검색 요청 중 문제가 발생했습니다.');
+      console.error(error);
+      Alert.alert('오류', '검색 요청 중 문제가 발생했습니다.');
     }
   };
 
